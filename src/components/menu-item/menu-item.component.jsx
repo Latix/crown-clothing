c2f983903217ea22364611ddf3bac2357d7179e0
Kamsi Kodi <kamsikodi@gmail.com>
2020-06-25 22:25:10 +0100

    Broken Homepage into smaller components

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -5,8 +5,11 @@ const MenuItem = ({ title, imageUrl, size }) => (
     <div style={{ 
         backgroundImage: `url(${imageUrl})` 
         }} className={`${size} menu-item`}>
+        <div className="background-image" style={{ 
+        backgroundImage: `url(${imageUrl})` 
+        }} />
         <div className="content">
-            <h1 className="title">{title}</h1>
+            <h1 className="title">{title.toUpperCase()}</h1>
             <span className="subtitle">Shop Name</span>
         </div>
     </div>
