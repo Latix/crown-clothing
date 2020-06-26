56cf24c99f6e1e42502ffebf289404d8849ea923
Kamsi Kodi <kamsikodi@gmail.com>
2020-06-26 10:05:44 +0100

    Integrated Google Auth

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import AuthPage from './pages/auth/auth.component';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends React.Component  {
   constructor(props) {
@@ -13,17 +13,30 @@ class App extends React.Component  {
 
     this.state = {
       currentUser: null
-    }
+    };
   }
 
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    auth.onAuthStateChanged(user =>  {
+    auth.onAuthStateChanged(async userAuth =>  {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+        userRef.onSnapshot(snapshot => {
+          this.setState({
+            currentUser: {
+              id: snapshot.id,
+              ...snapshot.data(),
+            }
+          })
+          console.log(this.state);
+        })
+      }
+      // console.log(userAuth);
+      // createUserProfileDocument(userAuth);
       this.setState({
-        currentUser: user,
-      });
-      console.log(user.displayName);
+        currentUser: userAuth
+      })
     })
   }
 
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -13,6 +13,7 @@ const Header = ({ currentUser }) => (
             <Link className="option" to='/shop'>Shop</Link>
             <Link className="option" to='/contact'>Contact</Link>
             {
+                
                 currentUser ? 
                 <div className="option" onClick={() => auth.signOut()}>Sign Out</div> : 
                 <Link className="option" to='/login'>Login</Link>
