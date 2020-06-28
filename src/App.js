0f10a1df52a26da4b2a901f91ce4f708a28513e8
Kamsi Kodi <kamsikodi@gmail.com>
2020-06-28 22:15:50 +0100

    Added Routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
@@ -48,6 +48,7 @@ class App extends React.Component  {
   componentWillUnmount() {
     this.unsubscribeFromAuth();
   }
+  
   render() {
     return (
         <div>
@@ -55,15 +56,18 @@ class App extends React.Component  {
           <Switch>
             <Route exact path='/' component={HomePage} />
             <Route path='/shop' component={ShopPage} />
-            <Route path='/login' component={AuthPage} />
+            <Route exact path='/login' render={() => this.props.currentUser ?  (<Redirect to="/" />) : ( <AuthPage />) } />
           </Switch>
         </div>
     );
   }
 }
 
+const mapStateToProps = (state) => ({
+  currentUser: state.user.currentUser
+})
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 })
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
